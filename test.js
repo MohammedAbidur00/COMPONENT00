7ffc32cb5f409eccc425376f87ec78b530774259
MohammedAbidur00 <abidurrahman788909@gmail.com>
2026-01-23 22:32:06 +0000

    commit 6: Added More Sections
    In this commit I added two new sections one called "wall of heads" and the an "about us" section. I also used classes for showing all the stuff in the containers such as the images and in the case of the "about us", also some text.

--- test.js
+++ test.js
@@ -112,8 +112,8 @@ class CarouselSets {
 
         this.SetsCarouselCon.classList.toggle("Sets-Carousel");
         this.SetsCarouselImg.classList.toggle("Sets-Carousel-Img");
-        this.SetsCarouselPrevBtn.classList.toggle("Set-Btn");
-        this.SetsCarouselNextBtn.classList.toggle("Set-Btn");
+        this.SetsCarouselPrevBtn.classList.toggle("Set-Btn-Carousel");
+        this.SetsCarouselNextBtn.classList.toggle("Set-Btn-Carousel");
 
         this.SetsCarouselTitle.innerText = this.InputTitle;
         this.SetsCarouselPrevBtn.innerText = "PREV";
@@ -213,6 +213,51 @@ class CarouselNavigator {
     }
 }
 
+class WallOfSomething {
+    constructor(InputArray, Container, ImgStyle, ImgContainer) {
+        this.InputArray = InputArray;
+        this.Container = Container;
+        this.ImgStyle = ImgStyle;
+        this.ImgContainer = ImgContainer;
+
+        this.Display(this.InputArray, this.Container, this.ImgStyle, this.ImgContainer);
+    }
+
+    Display(hArr, Con, ImgS, ImgC) {
+        this.text = "LEGO represents far more than a recreational pastime; it is a sophisticated medium where creativity, design, and engineering intersect with remarkable precision. I am especially drawn to the aesthetic depth of LEGO, as each set demonstrates a deliberate use of color, proportion, and structural harmony that closely mirrors principles found in architecture and industrial design. This visual and conceptual appeal becomes even more pronounced in the larger and more intricate sets, which transform the building process into an immersive and intellectually engaging experience.Beyond official releases, I hold a deep appreciation for custom LEGO sets created by master builders within the wider LEGO community. These creations often push the boundaries of scale, realism, and innovation, showcasing an exceptional level of technical skill and artistic vision. By reimagining familiar structures or inventing entirely original designs, master builders demonstrate the vast creative potential of LEGO as a platform for serious design work. Their ability to balance complexity with aesthetic coherence has been a continual source of inspiration.This sustained exposure to both official large-scale sets and community-driven custom builds ultimately motivated me to extend my interest beyond physical construction. It led me to create my own platform, referred to as website0, where I could document, showcase, and engage with LEGO-inspired ideas in a digital space. In this way, LEGO has influenced not only how I build, but also how I create, share, and communicate my appreciation for thoughtful design. \n\n\n- THE COMPONENT00 TEAM";
+        this.HeadsCon = Con;
+        for (let x = 0; x < hArr.length; x++) {
+            if (x === 7 && ImgC === "brickheads") {
+                this.AboutUsCon = document.createElement("div");
+                this.AboutUsCon.classList.toggle("AboutUsCon");
+                this.AboutUsTextCon = document.createElement("div");
+                this.AboutUsTitleCon = document.createElement("div");
+                this.AboutUsText = document.createElement("p");
+                this.AboutUsTitle = document.createElement("h2");
+                this.AboutUsTextCon.classList.toggle("AboutUsText");
+                this.AboutUsTitleCon.classList.toggle("Section-Title");
+                this.AboutUsText.innerText = this.text;
+                this.AboutUsTitle.innerText = "ABOUT US"
+                this.HeadsCon.appendChild(this.AboutUsCon);
+                this.AboutUsCon.appendChild(this.AboutUsTitleCon);
+                this.AboutUsCon.appendChild(this.AboutUsTextCon);
+                this.AboutUsTextCon.appendChild(this.AboutUsText);
+                this.AboutUsTitleCon.appendChild(this.AboutUsTitle);
+                this.HeadImg = document.createElement("img");
+                this.HeadsCon.appendChild(this.HeadImg);
+                this.HeadImg.classList.toggle(ImgS);
+                this.HeadImg.src = `assets/${ImgC}/${hArr[x]}.jpg`;
+            } else {
+                this.HeadImg = document.createElement("img");
+                this.HeadsCon.appendChild(this.HeadImg);
+                this.HeadImg.classList.toggle(ImgS);
+
+                this.HeadImg.src = `assets/${ImgC}/${hArr[x]}.jpg`;
+            }
+        }
+    }
+}
+
 const MinifigureImgArray = ["a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "a10", "a11", "a12", "a13", "a14", "a15", "a16", "a17", "a18", "a19", "a20", "a21", "a22", "a23", "a24", "a25", "a26", "a27", "a28", "a29", "a30", "a31", "a32", "a33", "a34", "a35", "a36", "a37", "a38", "a39", "a40", "a41", "a42", "a43", "a44"];
 const MinifigureTitleArray = ["ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "ELEVEN", "TWELVE", "THIRTEEN", "FOURTEEN", "FIFTEEN", "SIXTEEN", "SEVENTEEN", "EIGHTEEN", "NINETEEN", "TWENTY", "TWENTY ONE", "TWENTY TWO", "TWENTY THREE", "TWENTY FOUR", "TWENTY FIVE", "TWENTY SIX", "TWENTY SEVEN", "TWENTY EIGHT", "TWENTY NINE", "THIRTY", "THIRTY ONE", "THIRTY TWO", "THIRTY THREE", "THIRTY FOUR", "THIRTY FIVE", "THIRTY SIX", "THIRTY SEVEN", "THIRTY EIGHT", "THIRTY NINE", "FORTY", "FORTY ONE", "FORTY TWO", "FORTY THREE", "FORTY FOUR"];
 const Collection = new MinifigureSection(MinifigureTitleArray, MinifigureImgArray);
@@ -226,3 +271,20 @@ let CarouselSetSix;
 
 const ArrayOfObjects = [CarouselSetOne, CarouselSetTwo, CarouselSetThree, CarouselSetFour, CarouselSetFive];
 const CarouselNavArr = new CarouselNavigator(ArrayOfObjects);
+
+const HeadsArr = ["H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8"]
+const BrickHeadsArr = ["BH1", "BH2", "BH3", "BH4", "BH5", "BH6", "BH7", "BH8", "BH9", "BH10", "BH11", "BH12", "BH13", "BH14"];
+const WallOne = new WallOfSomething(HeadsArr, document.querySelector(".Heads-Container"), "Heads-Img", "heads");
+const WallTwo = new WallOfSomething(BrickHeadsArr, document.querySelector(".Brick-Heads-Container"), "Brick-Heads-Img", "brickheads");
+
+let collectiveBtn = document.getElementById("CollectiveBtn");
+let mainWrapper = document.getElementById("MainWrapper");
+let Refresh = document.getElementById("Refresh");
+
+collectiveBtn.addEventListener("click", () => {
+    mainWrapper.scrollIntoView({behavior: "smooth"})
+})
+
+Refresh.addEventListener("click", () => {
+    location.reload();
+})
